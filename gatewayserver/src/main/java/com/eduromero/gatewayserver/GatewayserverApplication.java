b18cad86b5d139ba12341334a45a92e0dea05b03
Edu Romero <eduardojrl927@gmail.com>
2026-06-09 23:34:40 +0200

    custom gateway routing

--- gatewayserver/src/main/java/com/eduromero/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eduromero/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,32 @@ package com.eduromero.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator evilAssBankingRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/evilassbanking/accounts/**")
+						.filters(f -> f.rewritePath("/evilassbanking/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/evilassbanking/cards/**")
+						.filters(f -> f.rewritePath("/evilassbanking/cards/(?<segment>.*)","/${segment}"))
+						.uri("lb://CARDS"))
+				.route(p -> p
+						.path("/evilassbanking/loans/**")
+						.filters(f -> f.rewritePath("/evilassbanking/loans/(?<segment>.*)","/${segment}"))
+						.uri("lb://LOANS")).build();
 	}
 
 }
